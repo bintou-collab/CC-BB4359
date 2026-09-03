f62b938398b87b380179b727ff6a4b7d5e9c80eb
bintou-collab <bb4359@nyu.edu>
2026-09-02 20:51:21 -0400

    Update sketch.js

--- week1/sketch.js
+++ week1/sketch.js
@@ -2,7 +2,7 @@
 // this is the only part of the file you need to edit!
 
 const quotes = [
-  { text: "the intersection of programming, design, and art", source: "Scott" },
+  { text: "write your own definition", source: "name of source" },
   { text: "a chance for code to be wrong", source: "Sara" },
   { text: "surprise, exploration, and aesthetic exploration", source: "Sally" },
   { text: "process over results", source: "Sam" },
